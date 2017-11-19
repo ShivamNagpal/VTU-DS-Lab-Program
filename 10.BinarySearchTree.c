5fbc99e74338ab06fa96c0a8c5d87fa92c3739dd
A-PC\shivam <shivamnagpal432@gmail.com>
2017-11-19 12:08:35 +0530

    rename

--- 10.BinarySearchTree.c
+++ 10.BinarySearchTree.c
@@ -1,5 +1,5 @@
 /*
-Design, Develop and Implement a menu driven Program in C for the following operations
+10. Design, Develop and Implement a menu driven Program in C for the following operations
 on Binary Search Tree (BST) of Integers
 a. Create a BST of N Integers: 6, 9, 5, 2, 8, 15, 24, 14, 7, 8, 5, 2
 b. Traverse the BST in Inorder, Preorder and Post Order
