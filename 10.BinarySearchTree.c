8d442ffd06828e9992df2307f94eab7c00dbec25
Shivam Nagpal <shivamnagpal432@gmail.com>
2017-11-21 20:24:30 +0530

    t

--- 10.BinarySearchTree.c
+++ 10.BinarySearchTree.c
@@ -13,9 +13,9 @@ e. Exit
 
 typedef struct t_node
 {
-    char data;
-    struct t_node *llink;
-    struct t_node *rlink;
+	char data;
+	struct t_node *llink;
+	struct t_node *rlink;
 } TNODE;
 
 void inorder(TNODE *root);
@@ -29,137 +29,137 @@ TNODE *findMin(TNODE *root);
 
 void main()
 {
-    TNODE *root=NULL;
-    int ch, flag;
-    int ele, key;
-    for(;;)
-    {
-        printf("\n1.Insert\n2.Inorder\n3.Preorder\n4.Postorder\n5.Search\n6.Delete\n7.Exit\n");
-        printf("Enter your choice\n");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:	printf("Enter the element to be inserted\n");
-                    scanf("%d",&ele);
-                    root=insert(root,ele);
-                    break;
+	TNODE *root=NULL;
+	int ch, flag;
+	int ele, key;
+	for(;;)
+	{
+		printf("\n1.Insert\n2.Inorder\n3.Preorder\n4.Postorder\n5.Search\n6.Delete\n7.Exit\n");
+		printf("Enter your choice\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:	printf("Enter the element to be inserted\n");
+					scanf("%d",&ele);
+					root=insert(root,ele);
+					break;
 					
-            case 2:	if(root==NULL)
-                        printf("Empty tree\n");
-                    else
-                    {
-                        printf("The contents are\n");
-                        inorder(root);
-                    }
-                    break;
-					
-            case 3:	if(root==NULL)
-                        printf("Empty tree\n");
-                    else
-                    {
-                        printf("The contents are\n");
-                        preorder(root);
-                    }
-                    break;
-					
-            case 4: if(root==NULL)
+			case 2:	if(root==NULL)
 						printf("Empty tree\n");
 					else
-                    {
-                        printf("The contents are\n");
-                        postorder(root);
-                    }
-                    break;
+					{
+						printf("The contents are\n");
+						inorder(root);
+					}
+					break;
 					
-            case 5:	printf("Enter the element to be searched\n");
+			case 3:	if(root==NULL)
+						printf("Empty tree\n");
+					else
+					{
+						printf("The contents are\n");
+						preorder(root);
+					}
+					break;
+					
+			case 4: if(root==NULL)
+						printf("Empty tree\n");
+					else
+					{
+						printf("The contents are\n");
+						postorder(root);
+					}
+					break;
+					
+			case 5:	printf("Enter the element to be searched\n");
 					scanf("%d",&key);
-                    flag=search(root, key);
-                    if(flag)
-                        printf("Found\n");
-                    else
-                        printf("Not Found\n");
-                    break;
+					flag=search(root, key);
+					if(flag)
+						printf("Found\n");
+					else
+						printf("Not Found\n");
+					break;
 					
 			case 6:	printf("Enter the element to be deleted\n");
 					scanf("%d", &ele);
 					root = delete(root, ele);
 					break;
 					
-            case 7:	exit(0);
-        }
-    }
+			case 7:	exit(0);
+		}
+	}
 }
 
 TNODE *getnode()
 {
-    TNODE *temp=(TNODE *)malloc(sizeof(TNODE));
-    if(temp==NULL)
-    {
-        printf("No Memory\n");
-        exit(0);
-    }
-    return temp;
+	TNODE *temp=(TNODE *)malloc(sizeof(TNODE));
+	if(temp==NULL)
+	{
+		printf("No Memory\n");
+		exit(0);
+	}
+	return temp;
 }
 
 TNODE *insert(TNODE *root,int ele)
 {
-    TNODE *new=NULL;
-    if(root==NULL)
-    {
-     	new = getnode();
-     	new->data=ele;
- 	    new->rlink=new->llink=NULL;
-        return new;
-    }
-    if(ele>(root->data))
-        root->rlink=insert(root->rlink,ele);
-    if(ele<(root->data))
-        root->llink=insert(root->llink,ele);
-    return root;
+	TNODE *new=NULL;
+	if(root==NULL)
+	{
+		new = getnode();
+		new->data=ele;
+		new->rlink=new->llink=NULL;
+		return new;
+	}
+	if(ele>(root->data))
+		root->rlink=insert(root->rlink,ele);
+	if(ele<(root->data))
+		root->llink=insert(root->llink,ele);
+	return root;
 }
 
 void inorder(TNODE *root)
 {
-    if(root!=NULL)
-    {
-        inorder(root->llink);
-        printf("%d ",root->data);
-        inorder(root->rlink);
-    }
+	if(root!=NULL)
+	{
+		inorder(root->llink);
+		printf("%d ",root->data);
+		inorder(root->rlink);
+	}
 }
 
 void preorder(TNODE *root)
 {
-    if(root!=NULL)
-    {
-        printf("%d ",root->data);
-        preorder(root->llink);
-        preorder(root->rlink);
-    }
+	if(root!=NULL)
+	{
+		printf("%d ",root->data);
+		preorder(root->llink);
+		preorder(root->rlink);
+	}
 }
 
 void postorder(TNODE *root)
 {
-    if(root!=NULL)
-    {
-        postorder(root->llink);
-        postorder(root->rlink);
-        printf("%d ",root->data);
-    }
+	if(root!=NULL)
+	{
+		postorder(root->llink);
+		postorder(root->rlink);
+		printf("%d ",root->data);
+	}
 }
 
 int search(TNODE *root,int key)
 {
-    if(root!=NULL)
-    {
-        if(key==root->data)
-            return 1;
-        if(key>root->data)
-            return(search(root->rlink,key));
-        if(key<root->data)
-            return(search(root->llink,key));
-    }
-    return 0;
+	if(root!=NULL)
+	{
+		if(key==root->data)
+			return 1;
+		if(key>root->data)
+			return(search(root->rlink,key));
+		if(key<root->data)
+			return(search(root->llink,key));
+	}
+	return 0;
 }
 
 TNODE *delete(TNODE *root, int ele)
@@ -181,12 +181,12 @@ TNODE *delete(TNODE *root, int ele)
 		}
 		else
 		{
-				temp = root;
-				if(root->llink == NULL)
-					root = root->rlink;
-				else if(root->rlink == NULL)
-					root = root->llink;
-				free(temp);
+			temp = root;
+			if(root->llink == NULL)
+				root = root->rlink;
+			else if(root->rlink == NULL)
+				root = root->llink;
+			free(temp);
 		}
 	}
 	return root;
